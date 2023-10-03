336f40af8e9780f98ec35148fec85f342c7c9166
saile515 <elias.jorgensen2006@gmail.com>
2023-10-03 14:11:27 +0200

    create projection matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "matrix.hpp"
+#include "parser.hpp"
 #include "vector.hpp"
 #include <iostream>
 
 int main(int argc, char **argv) {
-    Matrix4x4 mat1 = Matrix4x4::perspective(90, 0.1, 1000);
-    Matrix4x4 mat2 = Matrix4x4::orthographic(-1, 1, -1, 1, 0.1, 1000);
-    Matrix4x4 mat3 = mat1 * mat2;
-    std::cout << "Hello World\n";
+    Parser scene_parser(argv[1]);
+    scene_parser.parse();
+    SceneProperties scene = scene_parser.result;
+
+    Matrix4x4 projection_matrix;
+    if (scene.camera.type == "perspective") {
+        projection_matrix = Matrix4x4::perspective(scene.camera.fov, scene.camera.width, scene.camera.height, scene.camera.near, scene.camera.far);
+    } else if (scene.camera.type == "orthographic") {
+        projection_matrix = Matrix4x4::orthographic(0, scene.camera.width, scene.camera.height, 0, scene.camera.near, scene.camera.far);
+    }
     return 0;
 }
