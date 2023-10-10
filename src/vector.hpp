f15bc4d40227e7bff28c6bd6198e56319ad32712
saile515 <elias.jorgensen2006@gmail.com>
2023-10-10 15:06:55 +0200

    fix build issue

--- src/vector.hpp
+++ src/vector.hpp
@@ -9,19 +9,19 @@ Vector4 operator*(const Matrix4x4 &a, const Vector4 &b);
 
 class Vector3 {
 public:
-    Vector3(std::array<double, 3> = std::array<double, 3>({0, 0, 0}));
+    Vector3(std::array<double, 3> data = std::array<double, 3>({0, 0, 0}));
     [[nodiscard]] const double &operator[](int index) const;
 
 private:
-    double data[3];
+    std::array<double, 3> data;
 };
 
 class Vector4 {
 public:
-    Vector4(double data[4] = new double[4]{0});
+    Vector4(std::array<double, 4> data = std::array<double, 4>({0, 0, 0, 0}));
     Vector4(Vector3 vector, double w);
     [[nodiscard]] const double &operator[](int index) const;
 
 private:
-    double data[4];
+    std::array<double, 4> data;
 };
