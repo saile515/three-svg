8b4765a16f2b58e0ae06a5b99fa8a81c386c7b8d
saile515 <elias.jorgensen2006@gmail.com>
2023-11-21 09:07:40 +0100

    add missing empty line after '/home/saile515/Documents/projects/three-svg/src/parser.hpppragma once'

--- src/model.hpp
+++ src/model.hpp
@@ -1,11 +1,13 @@
 #pragma once
+
 #include "tiny_obj_loader.h"
 #include <string>
 #include <vector>
 
 class Model {
 public:
-    Model(std::vector<tinyobj::real_t> vertices, std::vector<tinyobj::real_t> colors, std::vector<tinyobj::real_t> normals, std::vector<tinyobj::index_t> indices);
+    Model(std::vector<tinyobj::real_t> vertices, std::vector<tinyobj::real_t> colors, std::vector<tinyobj::real_t> normals,
+          std::vector<tinyobj::index_t> indices);
     [[nodiscard]] static std::vector<Model> load_from_obj(std::string path, std::string mtl_path);
 
     std::vector<tinyobj::real_t> vertices;
